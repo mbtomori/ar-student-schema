3b396ef162646199607c629146633337fbbf6dbf
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-10 17:33:51 -0700

    changed migrate and students file

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,13 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |student_info|
+      student_info.string :first_name
+      student_info.string :last_name
+      student_info.string :gender
+      student_info.date :birthday
+      student_info.string :email
+      student_info.string :phone
+    end# HINT: checkout ActiveRecord::Migration.create_table
   end
 end
