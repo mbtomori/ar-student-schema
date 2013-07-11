b1682d9009d40a1976276fdef5bc53b67d6ea7ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-10 18:17:45 -0700

    validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,12 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
   
+  attr_reader :age
+
+  validates_uniqueness_of :email
+  validates :email, presence: true, format: { with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i} 
+  validates_length_of :phone, :in => 9..32
+  validates_numericality_of :age, :greater_than => 5
 
   def name
     first_name + " " + last_name
